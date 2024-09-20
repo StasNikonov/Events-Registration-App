1d6d37300086ca609d5001f67ae1045fc28174b7
Nikonov Stanislav <nikonoff.stanislav2005@gmail.com>
2024-09-21 00:29:56 +0300

    update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 const pool = new Pool({
   user: "postgres",
-  password: "18252613Dd",
+  password: "1234",
   host: "localhost",
   port: 5432,
   database: "node_postgres",
